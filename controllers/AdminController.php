ec9e3f9a6b6e6e60050b9c962ae8a6226e72ddc6
denilsonali2020 <denilson32@gmail.com>
2025-08-07 14:14:59 -0600

    Corrige validación de fecha y agrega exit después de header

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -6,47 +6,47 @@ use Model\AdminCita;
 use MVC\Router;
 
 class AdminController {
-    public static function index(Router $router) {
+    // public static function index(Router $router) {
         
-        if(session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
+    //     if(session_status() === PHP_SESSION_NONE) {
+    //         session_start();
+    //     }
 
-        isAdmin();
+    //     isAdmin();
         
-        $fecha = $_GET['fecha'] ?? date('Y-m-d');
-        $fechas = explode('-', $fecha);
+    //     $fecha = $_GET['fecha'] ?? date('Y-m-d');
+    //     $fechas = explode('-', $fecha);
 
-        if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $fecha)) {
-            header('Location: /404');
-            exit;
-        }
+    //     if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $fecha)) {
+    //         header('Location: /404');
+    //         exit;
+    //     }
         
-        if( !checkdate( $fechas[1], $fechas[2], $fechas[0]) ) {
-            header('Location: /404');
-            exit;
-        }         
+    //     if( !checkdate( $fechas[1], $fechas[2], $fechas[0]) ) {
+    //         header('Location: /404');
+    //         exit;
+    //     }         
                 
 
-        //consultar la base de datos
-        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
-        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
-        $consulta .= " FROM citas  ";
-        $consulta .= " LEFT OUTER JOIN usuarios ";
-        $consulta .= " ON citas.usuarioId=usuarios.id  ";
-        $consulta .= " LEFT OUTER JOIN citasServicios ";
-        $consulta .= " ON citasServicios.citaId=citas.id ";
-        $consulta .= " LEFT OUTER JOIN servicios ";
-        $consulta .= " ON servicios.id=citasServicios.servicioId ";
-        $consulta .= " WHERE fecha =  '{$fecha}' ";
+    //     //consultar la base de datos
+    //     $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
+    //     $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
+    //     $consulta .= " FROM citas  ";
+    //     $consulta .= " LEFT OUTER JOIN usuarios ";
+    //     $consulta .= " ON citas.usuarioId=usuarios.id  ";
+    //     $consulta .= " LEFT OUTER JOIN citasServicios ";
+    //     $consulta .= " ON citasServicios.citaId=citas.id ";
+    //     $consulta .= " LEFT OUTER JOIN servicios ";
+    //     $consulta .= " ON servicios.id=citasServicios.servicioId ";
+    //     $consulta .= " WHERE fecha =  '{$fecha}' ";
 
-        $citas = AdminCita::SQL($consulta);        
+    //     $citas = AdminCita::SQL($consulta);        
 
 
-        $router->render('admin/index', [
-            'nombre' => $_SESSION['nombre'],
-            'citas' => $citas,
-            'fecha' => $fecha
-        ]);
-    }
+    //     $router->render('admin/index', [
+    //         'nombre' => $_SESSION['nombre'],
+    //         'citas' => $citas,
+    //         'fecha' => $fecha
+    //     ]);
+    // }
 }
