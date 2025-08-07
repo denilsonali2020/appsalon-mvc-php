5af606a40b607157df6c072facff326573b84220
denilsonali2020 <denilson32@gmail.com>
2025-08-07 14:14:04 -0600

    Corrige validación de fecha y agrega exit después de header

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -13,7 +13,7 @@ class CitaController {
             session_start();
         }
 
-        //isAuth();//verificamos si esta autenticado si no lo esta no puede acceder a /citas
+        isAuth();//verificamos si esta autenticado si no lo esta no puede acceder a /citas
 
         $router->render('cita/index', [
             'nombre' => $_SESSION['nombre'],
